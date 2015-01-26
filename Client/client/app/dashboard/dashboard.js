ef7f4f7361a8eb61e1c38d44736f9d5a485f7a33
Trent Jones <trentjones21@gmail.com>
2015-01-26 13:08:56 -0700

    Added a dashboard view that the login page redirects to
    Gave the navbar its own style page

---
+++ Client/client/app/dashboard/dashboard.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('schejewelApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('dashboard', {
+        url: '/dashboard',
+        templateUrl: 'app/dashboard/dashboard.html',
+        controller: 'DashboardCtrl'
+      });
+  });
--- Client/client/app/login/login.controller.js
+++ Client/client/app/login/login.controller.js
@@ -1,13 +1,19 @@
+/* global alert */
 'use strict';
 
 angular.module('schejewelApp')
-    .controller('LoginCtrl', function($scope, $http) {
+    .controller('LoginCtrl', function($scope, $http, $location) {
         $scope.validate = function() {
-            $http.post('http://104.131.170.128:8080/alaska-excursions/api/user/validate', {
-                email: $scope.email,
-                password: $scope.password
-            }).then(function(result) {
-                console.log('result', result);
-            });
+            if ($scope.email && $scope.password) {
+                $http.post('http://104.131.170.128:8080/alaska-excursions/api/user/validate', {
+                    email: $scope.email,
+                    password: $scope.password
+                }).success(function(result) {
+                    alert(JSON.stringify(result));
+                    $location.path('/dashboard');
+                }).error(function(data, status) {
+                    alert('Error.  Check console: ' + JSON.stringify(status));
+                });
+            }
         };
     });
