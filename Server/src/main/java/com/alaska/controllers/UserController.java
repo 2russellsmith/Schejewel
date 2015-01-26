ddb9a949249535df944ff638304ee86a015a2bb6
kentbarker <kentbarker88@gmail.com>
2015-01-26 14:27:32 -0700

    Update UserController.java
    inlined a few helper functions and changed some modifiers to private of the functions that I just added to support hashing

--- Server/src/main/java/com/alaska/controllers/UserController.java
+++ Server/src/main/java/com/alaska/controllers/UserController.java
@@ -62,18 +62,14 @@ public class UserController{
             throw new  InvalidUserLoginException();
         }
     }
-    
-    public static String createHash(String password) throws NoSuchAlgorithmException, InvalidKeySpecException{
-        return createHash(password.toCharArray());
-    }
-    public static String createHash(char[] password) throws NoSuchAlgorithmException, InvalidKeySpecException{
+    private static String createHash(String password) throws NoSuchAlgorithmException, InvalidKeySpecException{
         // Generate a random salt
         SecureRandom random = new SecureRandom();
         byte[] salt = new byte[SALT_BYTE_SIZE];
         random.nextBytes(salt);
 
         // Hash the password
-        byte[] hash = pbkdf2(password, salt, PBKDF2_ITERATIONS, HASH_BYTE_SIZE);
+        byte[] hash = pbkdf2(password.toCharArray(), salt, PBKDF2_ITERATIONS, HASH_BYTE_SIZE);
         // format iterations:salt:hash
         return PBKDF2_ITERATIONS + ":" + toHex(salt) + ":" +  toHex(hash);
     }
@@ -82,10 +78,7 @@ public class UserController{
         SecretKeyFactory skf = SecretKeyFactory.getInstance(PBKDF2_ALGORITHM);
         return skf.generateSecret(spec).getEncoded();
     }
-    public static boolean validatePassword(String password, String correctHash) throws NoSuchAlgorithmException, InvalidKeySpecException{
-        return validatePassword(password.toCharArray(), correctHash);
-    }
-    public static boolean validatePassword(char[] password, String correctHash) throws NoSuchAlgorithmException, InvalidKeySpecException{
+    private static boolean validatePassword(String password, String correctHash) throws NoSuchAlgorithmException, InvalidKeySpecException{
         // Decode the hash into its parameters
         String[] params = correctHash.split(":");
         int iterations = Integer.parseInt(params[ITERATION_INDEX]);
@@ -93,7 +86,7 @@ public class UserController{
         byte[] hash = fromHex(params[PBKDF2_INDEX]);
         // Compute the hash of the provided password, using the same salt, 
         // iteration count, and hash length
-        byte[] testHash = pbkdf2(password, salt, iterations, hash.length);
+        byte[] testHash = pbkdf2(password.toCharArray(), salt, iterations, hash.length);
         // Compare the hashes in constant time. The password is correct if
         // both hashes match.
         return slowEquals(hash, testHash);
