3ac1e2d26c62318f40c8231dda2f224bdb6b012f
Trent Jones <trentjones21@gmail.com>
2015-01-26 13:58:35 -0700

    Changed the look of the navbar.

--- Client/client/app/dashboard/dashboard.controller.js
+++ Client/client/app/dashboard/dashboard.controller.js
@@ -1,6 +1,10 @@
 'use strict';
 
 angular.module('schejewelApp')
-  .controller('DashboardCtrl', function ($scope) {
-    $scope.message = 'Hello';
-  });
+    .controller('DashboardCtrl', function($scope) {
+        $scope.events = [{
+            title: 'Pick up group'
+        }, {
+            title: 'Go fishing'
+        }];
+    });
