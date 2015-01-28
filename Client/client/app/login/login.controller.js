1025c23784af58cc80d077d2d8753661d1254514
Trent Jones <trentjones21@gmail.com>
2015-01-27 17:00:57 -0700

    login

--- Client/client/app/login/login.controller.js
+++ Client/client/app/login/login.controller.js
@@ -1,19 +1,21 @@
-/* global alert */
+/* global */
 'use strict';
 
 angular.module('schejewelApp')
     .controller('LoginCtrl', function($scope, $http, $location) {
         $scope.validate = function() {
-            if ($scope.email && $scope.password) {
-                $http.post('http://104.131.170.128:8080/alaska-excursions/api/user/validate', {
-                    email: $scope.email,
-                    password: $scope.password
+            // if ($scope.email && $scope.password) {
+                $http.get('http://104.131.170.128:8080/alaska-excursions/api/login', {
+                    headers: {
+                        'Authorization': 'Basic ZW5jcnlwdEBnbWFpbC5jb206MTIzNA=='
+                    }
                 }).success(function(result) {
-                    alert(JSON.stringify(result));
-                    $location.path('/dashboard');
+                    console.log(JSON.stringify(result));
+                    // $location.path('/dashboard');
                 }).error(function(data, status) {
-                    alert('Error.  Check console: ' + JSON.stringify(status));
+                    console.log('data', data);
                 });
-            }
+            // }
+
         };
     });
