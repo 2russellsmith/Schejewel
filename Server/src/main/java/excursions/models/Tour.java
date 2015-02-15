e86ac21cac9601c2f1f1aca7c0c1a89c69173f01
kentbarker <kentbarker88@gmail.com>
2015-02-14 20:20:02 -0700

    wip.  added dates
    will come back soon to add tour types and groups after I have built those models

--- Server/src/main/java/excursions/models/Tour.java
+++ Server/src/main/java/excursions/models/Tour.java
@@ -1,5 +1,34 @@
 package excursions.models;
 
+import java.util.Date;
+
 public class Tour {
-    //Todo: This needs to be implemented
+    private Date StartDate;
+    private Date EndDate;
+    
+    //we can change the granularity of the time we set it with.
+    //default is milliseconds, but I don't think we need that kind
+    //of accuracy.  If we need minutes, or hours instead, that's just
+    //a matter of changing the multiplier in this function
+    //btw, the seconds is seconds from "now" as in, when this function
+    //was called.  there is probably a better way to handle all of this
+    //but until we know more about how we want to do this, I figured this
+    //was good enough
+    public void setStartDate(int seconds){
+        Date today = new Date();
+        StartDate = new Date(today.getTime() + seconds * 1000);
+    }
+    public Date getStartDate(){
+        return StartDate;
+    }
+    
+    //This function requires startDate to be set first, that can
+    //be changed if it needs to be.  this way forces the end date
+    //to be after the start date
+    public void setEndDate(int seconds){
+        EndDate = new Date(StartDate.getTime() + seconds * 1000);
+    }
+    public Date getEndDate(){
+        return EndDate;
+    }
 }
