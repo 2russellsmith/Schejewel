0453449504866cf9f026a7ea0549938ee8188716
kentbarker <kentbarker88@gmail.com>
2015-02-14 21:22:51 -0700

    Update CruiseLine.java

--- Server/src/main/java/excursions/models/CruiseLine.java
+++ Server/src/main/java/excursions/models/CruiseLine.java
@@ -1,5 +1,15 @@
 package excursions.models;
 
+//this wasn't in the schema, and looking at the data base,
+//this seems to just be a name
+
 public class CruiseLine {
-    //Todo: This needs to be implemented
+    private String Name;
+    
+    public void setName(String Name){
+        this.Name = Name;
+    }
+    public String getName(){
+        return Name;
+    }
 }
