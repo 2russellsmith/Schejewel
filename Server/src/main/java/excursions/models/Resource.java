2677c022290b80eb2d3e9edf5ac15dd039f03b6f
kentbarker <kentbarker88@gmail.com>
2015-02-14 20:01:35 -0700

    Added variables and getters/setters

--- Server/src/main/java/excursions/models/Resource.java
+++ Server/src/main/java/excursions/models/Resource.java
@@ -1,5 +1,36 @@
 package excursions.models;
 
 public class Resource {
-    //Todo: This needs to be implemented
+    private String ID;
+    private String Name;
+    private int Capacity;
+    private String Owner;
+    
+    public void setID(String ID){
+        this.ID = ID;
+    }
+    public String getID(){
+        return ID;
+    }
+    
+    public void setName(String Name){
+        this.Name = Name;
+    }
+    public String getName(){
+        return Name;
+    }
+    
+    public void setCapacity(int Capacity){
+        this.Capacity = Capacity;
+    }
+    public int getCapacity(){
+        return Capacity;
+    }
+    
+    public void setOwner(String Owner){
+        this.Owner = Owner;
+    }
+    public String getOwner(){
+        return Owner;
+    }
 }
