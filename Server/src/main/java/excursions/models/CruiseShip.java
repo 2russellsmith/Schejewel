d4078b0cc062016a546428a3c6cd5e1cae86ec1a
kentbarker <kentbarker88@gmail.com>
2015-02-14 20:45:44 -0700

    Added variables and getters/setters

--- Server/src/main/java/excursions/models/CruiseShip.java
+++ Server/src/main/java/excursions/models/CruiseShip.java
@@ -1,5 +1,60 @@
 package excursions.models;
 
+import java.util.Date;
+
+
+//variables that I was unsure about in the schema are "included" as comments
 public class CruiseShip {
-    //Todo: This needs to be implemented
+    //cruiseLine
+    private Date ArrivalDate;
+    private Date DepartureDate;
+    private String Name;
+    private int PassengerCount;
+    private boolean AllAboard;
+    //Dock
+    //Voyage
+    private boolean Settled;
+    
+    public Date getArrivalDate() {
+		return ArrivalDate;
+	}
+	public void setArrivalDate(int seconds) {
+	    Date today = new Date();
+		this.ArrivalDate = new Date(today.getTime() + seconds * 1000);
+	}
+	
+	public Date getDepartureDate() {
+		return DepartureDate;
+	}
+	public void setDepartureDate(int seconds) {
+		this.DepartureDate = new Date(ArrivalDate.getTime() + seconds * 1000);
+	}
+	
+	public String getName() {
+		return Name;
+	}
+	public void setName(String Name) {
+		this.Name = Name;
+	}
+	
+	public int getPassengerCount() {
+		return PassengerCount;
+	}
+	public void setPassengerCount(int PassengerCount) {
+		this.PassengerCount = PassengerCount;
+	}
+	
+	public boolean getAllAboard() {
+		return AllAboard;
+	}
+	public void setAllAboard(boolean AllAboard) {
+		this.AllAboard = AllAboard;
+	}
+	
+	public boolean getSettled() {
+		return Settled;
+	}
+	public void setSettled(boolean settled) {
+		Settled = settled;
+	}
 }
