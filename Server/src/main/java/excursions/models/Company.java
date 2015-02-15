aba6cea474bf69d281a556e326087fe51355f13a
kentbarker <kentbarker88@gmail.com>
2015-02-14 20:31:41 -0700

    Added variables and getters/setters
    this needs to be changed if JSON doesn't know how to auto create an ArrayList.

--- Server/src/main/java/excursions/models/Company.java
+++ Server/src/main/java/excursions/models/Company.java
@@ -1,5 +1,29 @@
 package excursions.models;
 
+//does this need to import Resources and Tours?
+//I think it's fine because they are in the same
+//package.  Projects that I run on my environment
+//don't need imports if it's referencing things from
+//the same package
+import java.util.ArrayList;
+
 public class Company {
-    //Todo: This needs to be implemented
+    private ArrayList<Resource> Resources;
+    private ArrayList<Tour> Tours;
+    
+    //I suspect that JSON doesn't know how to do this, so this
+    //will probably need to be changed
+    public void setResources(ArrayList<Resource> Resources){
+        this.Resources = new ArrayList<Resource>(Resources);
+    }
+    public ArrayList<Resource> getResources(){
+        return Resources;
+    }
+    
+    public void setTours(ArrayList<Tour> Tours){
+        this.Tours = new ArrayList<Tour>(Tours);
+    }
+    public ArrayList<Tour> getTours(){
+        return Tours;
+    }
 }
