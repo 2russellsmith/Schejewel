a6f52df8b104a086b1980a3ce9436b183bf72685
kentbarker <kentbarker88@gmail.com>
2015-02-18 22:38:22 -0700

    added better date control
    setting the date makes more sense now hopefully.  it also doesn't use deprecated functions

--- Server/src/main/java/excursions/models/Tour.java
+++ Server/src/main/java/excursions/models/Tour.java
@@ -1,6 +1,7 @@
 package excursions.models;
 
 import java.util.Date;
+import java.util.Calendar;
 
 //commented out variables were ones that I couldn't understand even after looking at the database
 public class Tour {
@@ -9,27 +10,36 @@ public class Tour {
     private int TourTypes;// from my best understanding of the database
     //groups //tourtypeid + ownerid//from the database
     
-    //we can change the granularity of the time we set it with.
-    //default is milliseconds, but I don't think we need that kind
-    //of accuracy.  If we need minutes, or hours instead, that's just
-    //a matter of changing the multiplier in this function
-    //btw, the seconds is seconds from "now" as in, when this function
-    //was called.  there is probably a better way to handle all of this
-    //but until we know more about how we want to do this, I figured this
-    //was good enough
-    public void setStartDate(int seconds){
-        Date today = new Date();
-        StartDate = new Date(today.getTime() + seconds * 1000);
+    public void setStartDate(int year, int month, int day, int hour, int minute){
+        //numbers roll over, they don't throw errors.  i.e. 74 minutes will be 
+        //translated to 1 more hour and 14 minutes
+        Calendar toSet = Calendar.getInstance();
+        
+        toSet.set(Calendar.YEAR, year);
+		toSet.set(Calendar.MONTH, month);//0 based.  0 - 11
+		toSet.set(Calendar.DATE, day);
+		toSet.set(Calendar.HOUR_OF_DAY, hour);
+		toSet.set(Calendar.MINUTE, minute);
+		
+		StartDate = toSet.getTime();
     }
     public Date getStartDate(){
         return StartDate;
     }
-    
-    //This function requires startDate to be set first, that can
-    //be changed if it needs to be.  this way forces the end date
-    //to be after the start date
-    public void setEndDate(int seconds){
-        EndDate = new Date(StartDate.getTime() + seconds * 1000);
+
+    public void setEndDate(int year, int month, int day, int hour, int minute){
+        //numbers roll over, they don't throw errors.  i.e. 74 minutes will be 
+        //translated to 1 more hour and 14 minutes
+        Calendar toSet = Calendar.getInstance();
+        
+        toSet.set(Calendar.YEAR, year);
+		toSet.set(Calendar.MONTH, month);//0 based.  0 - 11
+		toSet.set(Calendar.DATE, day);
+		toSet.set(Calendar.HOUR_OF_DAY, hour);
+		toSet.set(Calendar.MINUTE, minute);
+		
+		EndDate = toSet.getTime();
+		//if(StartDate.compareTo(EndDate) != -1) //then error.  start date should be before end date
     }
     public Date getEndDate(){
         return EndDate;
