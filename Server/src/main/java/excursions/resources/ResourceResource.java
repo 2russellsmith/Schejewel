61b907a9d993280f5d1c3b4ac760398c131a92bf
Kent Barker <kentbarker88@gmail.com>
2015-02-20 19:48:02 -0700

    initial commit

--- Server/src/main/java/excursions/resources/ResourceResource.java
+++ Server/src/main/java/excursions/resources/ResourceResource.java
@@ -1,11 +1,44 @@
 package excursions.resources;
 
+import java.util.List;
+
 import excursions.controllers.ResourceController;
+import excursions.models.Resource;
+import excursions.models.User;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ResourceResource {
     @Autowired
-    private ResourceController resourceController = new ResourceController();
+    private ResourceController resourceController = new ResourceController();   
+    
+    @RequestMapping(value = "/api/resource", method = RequestMethod.GET)
+    public ResponseEntity<List<Resource>> getResources(){
+        return new ResponseEntity<List<Resource>>(resourceController.getResources(), HttpStatus.FOUND);
+    }
+    @RequestMapping(value = "/api/resource", params = {"ResourceId"}, method = RequestMethod.GET)
+    public ResponseEntity<Resource> getResource(@RequestParam(value = "ResourceId") int ResourceId){
+        return new ResponseEntity<Resource>(resourceController.getResource(ResourceId), HttpStatus.FOUND);
+    } 
+    @RequestMapping(value = "/api/resource", method = RequestMethod.PUT)
+    public ResponseEntity<Resource> updateResource(@RequestBody Resource toUpdate){
+        return new ResponseEntity<Resource>(resourceController.updateResource(toUpdate), HttpStatus.OK);
+    }
+    @RequestMapping(value = "/api/resource", params = {"ResourceId"}, method = RequestMethod.DELETE)
+    public ResponseEntity<Resource> deleteResource(@RequestParam(value = "ResourceId") int ResourceId){
+        return new ResponseEntity<Resource>(resourceController.deleteResource(ResourceId), HttpStatus.FOUND);//this is null as of now.  don't try to access what this returns
+    }
+    @RequestMapping(value = "/api/resource", method = RequestMethod.POST)
+    public ResponseEntity<Resource> createResource(@RequestBody Resource toCreate){
+        return new ResponseEntity<Resource>(resourceController.createResource(toCreate), HttpStatus.CREATED);
+    }
+
 }
