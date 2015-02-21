a466f62add2e7631a0989a2aab6545b829763a21
Kent Barker <kentbarker88@gmail.com>
2015-02-20 20:41:02 -0700

    cleaned up imports

--- Server/src/main/java/excursions/resources/ResourceResource.java
+++ Server/src/main/java/excursions/resources/ResourceResource.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import excursions.controllers.ResourceController;
 import excursions.models.Resource;
-import excursions.models.User;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
