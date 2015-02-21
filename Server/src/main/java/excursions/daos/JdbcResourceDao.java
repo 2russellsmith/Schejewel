1a105c0b45d2d4e24c2c6e9ab68cf5d8d29bdb82
Kent Barker <kentbarker88@gmail.com>
2015-02-20 19:10:16 -0700

    added the resourceDao code

--- Server/src/main/java/excursions/daos/JdbcResourceDao.java
+++ Server/src/main/java/excursions/daos/JdbcResourceDao.java
@@ -1,9 +1,20 @@
 package excursions.daos;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import excursions.daos.interfaces.ResourceDao;
+import excursions.models.Resource;
+
 import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -14,6 +25,64 @@ public class JdbcResourceDao implements ResourceDao {
 	public void setDataSource(DataSource ds) {
 		jdbc = new NamedParameterJdbcTemplate(ds);
 	}
-	
-    //Todo: This needs to be implemented. WORK!
+
+	@Override
+	public List<Resource> getResources() {
+		List<Resource> resources = new ArrayList<>();
+		
+		String sql = "SELECT id,name,capacity,owner_id FROM resource";
+		SqlParameterSource paramSource = null;//TODO: does this work?
+		List<Map<String, Object>> rows = jdbc.queryForList(sql, paramSource );
+		
+		for (Map<String, Object> row : rows) {
+			Resource resource = new Resource();
+			resource.setId((Integer)(row.get("ID")));
+			resource.setName((String)row.get("NAME"));
+			resource.setCapacity((Integer)row.get("CAPACITY"));
+			resource.setOwner_id((Integer)row.get("OWNER_ID"));
+			resources.add(resource);
+		}
+		return resources;
+	}
+
+	@Override
+	public Resource getResource(int ResourceId) {
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("id",ResourceId);
+        String sql = "SELECT id,name,capacity,owner_id FROM resource WHERE id = :id";
+
+        Resource resource = jdbc.queryForObject(sql, params, new BeanPropertyRowMapper<>(Resource.class));
+        return resource;
+	}
+
+	@Override
+	public boolean updateResource(Resource toUpdate) {
+        BeanPropertySqlParameterSource params = new BeanPropertySqlParameterSource(toUpdate);
+        String sql = "UPDATE resource SET name = :name, capacity = :capacity WHERE id = :id";
+
+        jdbc.update(sql,params);
+        //if(error) return false;
+		return true;
+	}
+
+	@Override
+	public boolean deleteResource(int ResourceId) {
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("id",ResourceId);
+        String sql = "DELETE FROM resource WHERE id = :id";
+
+        jdbc.update(sql,params);
+        //if(error) return false;
+		return true;
+	}
+
+	@Override
+	public boolean createResource(Resource toCreate) {
+        BeanPropertySqlParameterSource params = new BeanPropertySqlParameterSource(toCreate);
+        String sql = "INSERT INTO resource(id,name,capacity,owner_id) VALUES(:id,:name,:capacity,:owner_id)";
+
+        jdbc.update(sql,params);
+        //if(error) return false;
+		return true;
+	}
 }
--- Server/src/main/java/excursions/daos/interfaces/ResourceDao.java
+++ Server/src/main/java/excursions/daos/interfaces/ResourceDao.java
@@ -1,7 +1,17 @@
 package excursions.daos.interfaces;
 
+import java.util.List;
+
 import javax.sql.DataSource;
+
+import excursions.models.Resource;
 
 public interface ResourceDao {
 	public void setDataSource(DataSource ds);
+	public List<Resource> getResources();
+	public Resource getResource(int ResourceId);
+	public boolean updateResource(Resource toUpdate);
+	public boolean deleteResource(int ResourceId);
+	public boolean createResource(Resource toCreate);
+	
 }
