b5f6c407217c41062988aef99b66dd7eaf7ed6e8
Kent Barker <kentbarker88@gmail.com>
2015-02-20 19:19:31 -0700

    initial code commit

--- Server/src/main/java/excursions/controllers/ResourceController.java
+++ Server/src/main/java/excursions/controllers/ResourceController.java
@@ -2,6 +2,8 @@ package excursions.controllers;
 
 import excursions.daos.JdbcResourceDao;
 import excursions.daos.interfaces.ResourceDao;
+import excursions.models.Resource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
@@ -9,4 +11,17 @@ import org.springframework.stereotype.Controller;
 public class ResourceController {
     @Autowired
     private ResourceDao resourceDao = new JdbcResourceDao();
+    
+    public Resource getResource(int ResourceId){
+    	return resourceDao.getResource(ResourceId);
+    }
+    public boolean updateResource(Resource toUpdate){
+    	return resourceDao.updateResource(toUpdate);
+    }
+    public boolean deleteResource(int ResourceId){
+    	return resourceDao.deleteResource(ResourceId);
+    }
+    public boolean createResrouce(Resource toCreate){
+    	return resourceDao.createResource(toCreate);
+    }
 }
