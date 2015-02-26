8caaefa83df22c0e5d05b8071f53ae8a9b5a9ca5
gwwerner <gwwerner@hotmail.com>
2015-02-26 14:07:46 -0700

    created the companyDao and tests

--- Server/src/main/java/excursions/daos/JdbcCompanyDao.java
+++ Server/src/main/java/excursions/daos/JdbcCompanyDao.java
@@ -1,9 +1,14 @@
 package excursions.daos;
 
 import excursions.daos.interfaces.CompanyDao;
+import excursions.models.Company;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -11,8 +16,48 @@ public class JdbcCompanyDao implements CompanyDao {
     @Autowired
     private NamedParameterJdbcTemplate jdbc;
 	
+	@Override
 	public void setDataSource(DataSource ds) {
 		jdbc = new NamedParameterJdbcTemplate(ds);
 	}
-    //Todo: This needs to be implemented
+    
+	@Override
+	public Company getCompany(int companyid) {
+		MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("id",companyid);
+        String sql = "SELECT * FROM company WHERE id = :id";
+        Company company = jdbc.queryForObject(sql, params, new BeanPropertyRowMapper<>(Company.class));
+		company.setCompaynId(companyid);
+        return company;
+	}
+
+	@Override
+	public Company updateCompany(Company company) {
+		MapSqlParameterSource params = new MapSqlParameterSource();
+		params.addValue("id",company.getCompanyId());
+        params.addValue("name",company.getName());
+		String sql = "UPDATE company SET name=:name WHERE id=:id";
+		jdbc.update(sql, params);
+		return company;
+	}
+
+	@Override
+	public void deleteCompany(int companyid) {
+		MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("id",companyid);
+		String sql = "DELETE FROM company WHERE id=:id";
+		jdbc.update(sql, params);
+	}
+
+	@Override
+	public Company createCompany(Company company) {
+		MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("name", company.getName());
+		String sql = "INSERT INTO company(name)"
+			+ " VALUES(:name)";
+		KeyHolder kh = new GeneratedKeyHolder();
+		jdbc.update(sql, params, kh);
+		company.setCompaynId(kh.getKey().intValue());
+		return company;
+	}
 }
--- Server/src/main/java/excursions/daos/interfaces/CompanyDao.java
+++ Server/src/main/java/excursions/daos/interfaces/CompanyDao.java
@@ -1,7 +1,12 @@
 package excursions.daos.interfaces;
 
+import excursions.models.Company;
 import javax.sql.DataSource;
 
 public interface CompanyDao {
 	public void setDataSource(DataSource ds);
+	public Company getCompany(int companyid);
+	public Company updateCompany(Company company);
+	public void deleteCompany(int companyid);
+	public Company createCompany(Company company);
 }
