e1bb4609c8062640ac620a52412d3e77a77fc9a9
Trent Jones <trentjones21@gmail.com>
2015-03-26 17:27:22 -0600

    switched ports

--- Client/server/config/environment/development.js
+++ Client/server/config/environment/development.js
@@ -3,11 +3,11 @@
 // Development specific configuration
 // ==================================
 module.exports = {
-    // MongoDB connection options
-    mongo: {
-        uri: 'mongodb://localhost/schejewel-dev'
-    },
-    port: 9000,
+	// MongoDB connection options
+	mongo: {
+		uri: 'mongodb://localhost/schejewel-dev'
+	},
+	port: 8080,
 
-    seedDB: true
+	seedDB: true
 };
