52118d85d0b81d3b3e1fcefcbea26f6d626907f8
Trent Jones <trentjones21@gmail.com>
2015-03-31 15:46:59 -0600

    removed deprecated call

--- Client/client/app/main/main.controller.js
+++ Client/client/app/main/main.controller.js
@@ -1,12 +1,7 @@
 'use strict';
 
 angular.module('schejewelApp')
-    .controller('MainCtrl', function($scope, $http) {
-        $scope.awesomeThings = [];
+	.controller('MainCtrl', function () {
 
-        // get a list of events from the api
-        $http.get('/api/events/today').success(function(events) {
-            $scope.events = events;
-        });
 
-    });
+	});
