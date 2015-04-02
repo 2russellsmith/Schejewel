209c28a3e500480a1065695aa5af46a24ad2dc51
RyanBaxter999 <endersgame999@gmail.com>
2015-04-02 14:32:26 -0600

    Fixed some of the small bugs in resourceView
    Corrected colors, and correctly pull data from resource endpoint. Just
    need to update the search every time the day changes.

--- Client/client/app/directives/schejewel.resourceView/schejewel.resourceView.directive.js
+++ Client/client/app/directives/schejewel.resourceView/schejewel.resourceView.directive.js
@@ -6,10 +6,15 @@ angular.module('schejewelApp')
 			templateUrl: 'app/directives/schejewel.resourceView/schejewel.resourceView.html',
 			restrict: 'EA',
 			link: function ($scope) {
-				var milliseconds = new Date();
-				Data.getResources(milliseconds).then(function(data) {
-            //console.log('data', data);
+				$scope.rawResourceData;
+				var dateString = "2015-05-05"; //TODO: getToday
+				Data.getResources(dateString).then(function(data) {
+					console.log(data);
+					$scope.rawResourceData = data;
+					$scope.convertDataToCalendar();
+					$scope.loadResourceButtons();
         });
+
 				$scope.resourceData =
 				[
 					{
@@ -27,7 +32,7 @@ angular.module('schejewelApp')
 				$scope.activeResources = [];
 				$scope.resourceList = [];
 
-				$scope.colors = ['#40e0d0', '#378006', '#ff0000', '#0000ff', '#008000', '#468499', '#800000', '#333333']
+				$scope.colors = ['#0099CC', '#378006', '#ff0000', '#40e0d0', '#008000', '#468499', '#800000', '#0000ff', '#333333']
 				//$scope.calendarData = {start:2015-03-29T20:41:02+00:00, end:2015-03-29T20:41:06+00:0};
 				$scope.eventList = {events:[]}
 		    $scope.events =
@@ -66,17 +71,34 @@ angular.module('schejewelApp')
 							]
 				}
 
-				$scope.convertDataToCalendar = function(){
-
+				$scope.addMinutes = function (time, minsToAdd) {
+				function D(J){ return (J<10? '0':'') + J;};
+				var piece = time.split(':');
+				var mins = piece[0]*60 + +piece[1] + +minsToAdd;
+				return D(mins%(24*60)/60 | 0) + ':' + D(mins%60);
 				}
 
-				$scope.createNewEventList = function(events){
+				$scope.convertDataToCalendar = function(){
+					//move/convert rawResourceData to resourceData
+					$scope.resourceData = []
+					for(var i = 0; i < $scope.rawResourceData.length; i++){
+						var newResourceEntry =  {};
+						newResourceEntry.title = $scope.rawResourceData[i].name;
+						newResourceEntry.start = $scope.rawResourceData[i].startDate + "T" + $scope.rawResourceData[i].startTime; //Weird date format uses 'T'
+						var endTime = $scope.addMinutes($scope.rawResourceData[i].startTime, String($scope.rawResourceData[i].duration))
+						newResourceEntry.end = $scope.rawResourceData[i].startDate + "T" + endTime + ":00";
+						$scope.resourceData.push(newResourceEntry);
+					}
+				}
+
+				$scope.createNewEventList = function(events, colorIndex){
 					$scope.eventList = {events: []};
 					for(var j = 0; j < events.length; j++){
-						for(var i = 0; i < $scope.events.events.length; i++){
-							if($scope.events.events[i].title === events[j]){
-								var event = $scope.events.events[i];
-								event.backgroundColor = $scope.colors[j];
+						for(var i = 0; i < $scope.resourceData.length; i++){
+							console.log($scope.resourceData[i].title + " "+ events[j].name);
+							if($scope.resourceData[i].title === events[j].name){
+								var event = $scope.resourceData[i];
+								event.backgroundColor = $scope.colors[events[j].color];
 								$scope.eventList.events.push(event);
 							}
 						}
@@ -84,11 +106,13 @@ angular.module('schejewelApp')
 					$('#calendar').fullCalendar('removeEvents')
 					$('#calendar').fullCalendar('removeEventSource', $scope.eventList);
 					$('#calendar').fullCalendar('addEventSource', $scope.eventList);
+					console.log($scope.eventList);
 					$('#calendar').fullCalendar('refetchEvents');
 				}
 
 				$scope.loadResourceButtons = function(){
 					for(var i = 0; i < $scope.resourceData.length; i++){
+						console.log($scope.resourceData[i]);
 						var newResource = true;
 						for(var j = 0; j < $scope.resourceList.length; j++){
 							if($scope.resourceData[i].title === $scope.resourceList[j]){
@@ -100,24 +124,23 @@ angular.module('schejewelApp')
 						}
 					}
 					for(var i = 0; i < $scope.resourceList.length; i++){
-						$('.buttonHolder').append('<button style="margin-bottom: 20px;" class="resourceButton" value="'+ $scope.resourceList[i] +'">'+ $scope.resourceList[i] +'</button>');
+						$('.buttonHolder').append('<button style="margin-bottom: 20px;" title = "' + i + '"class="resourceButton" value="'+ $scope.resourceList[i] +'">'+ $scope.resourceList[i] +'</button>');
 					}
 				}
-
-				angular.element(document).ready(function () {
-					$scope.loadResourceButtons()
-    		});
 
 				$(document).on( "click", '.resourceButton', function () {
 					var alreadyIn = false;
 					for(var i = 0; i < $scope.activeResources.length; i++){
-						if($scope.activeResources[i] === this.value){
+						if($scope.activeResources[i].name === this.value){
 							$scope.activeResources.splice(i, 1);
 							alreadyIn = true;
 						}
 					}
 					if(!alreadyIn){
-						$scope.activeResources.push(this.value);
+						var nameColorPair = {name: "", color:-1};
+						nameColorPair.name = this.value;
+						nameColorPair.color = this.title;
+						$scope.activeResources.push(nameColorPair);
 					}
 					$scope.createNewEventList($scope.activeResources);
 				});
@@ -129,65 +152,6 @@ angular.module('schejewelApp')
 					minTime: "06:00:00",
 					events: $scope.eventList
 				});
-
-				/*$scope.dummyData = ['6:00', '6:30', '7:00', '7:30', '8:00', '8:30', '9:00', '9:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '1:00', '1:30',
-					'2:00', '2:30', '3:00', '3:30', '4:00', '4:30', '5:00', '5:30'
-				];
-				$scope.Columns = [{
-					'resourceType': 'Bus1',
-					'useTimes': [{
-							'startTime': '11:00',
-							'endTime': '11:30'
-						},
-
-						{
-							'startTime': '13:00',
-							'endTime': '13:30'
-						}
-					]
-				}, {
-					'resourceType': 'Bus2',
-					'useTimes': [{
-							'startTime': '11:00',
-							'endTime': '11:30'
-						},
-
-						{
-							'startTime': '13:00',
-							'endTime': '13:30'
-						}
-					]
-				}];
-
-
-
-				$scope.calculateTimeStart = function (start) {
-					var a = start.split(':'); // split it at the colons
-
-					var minutes = (+a[0]) * 60 + (+a[1]) - 6 * 60 + 19;
-					return minutes;
-				};
-				$scope.calculateTimeDuration = function (start, end) {
-					var a = start.split(':'); // split it at the colons
-					var b = end.split(':'); // split it at the colons
-					var minutesA = (+a[0]) * 60 + (+a[1]);
-					var minutesB = (+b[0]) * 60 + (+b[1]);
-					var result = Number(minutesB) - Number(minutesA) - 1;
-					return result.toString();
-				};
-
-				$scope.getStyle = function (start, end) {
-					var response = {
-						'top': $scope.calculateTimeStart(start) + 'px',
-						'height': $scope.calculateTimeDuration(start, end) + 'px'
-					};
-					return response;
-				};
-
-				$scope.appear = function () {
-					console.log("Appearing");
-				};*/
-
 			}
 		};
 	});
--- Client/client/app/services/Data/Data.service.js
+++ Client/client/app/services/Data/Data.service.js
@@ -1,48 +1,39 @@
 'use strict';
 
 angular.module('schejewelApp')
-    .service('Data', function ($q, $http, Auth) {
-        var url =
-            'http://Default-Environment-69yjsdh6ez.elasticbeanstalk.com/api/';
-
-
+    .service('Data', function($q, $http, Auth) {
+        var url = 'http://Default-Environment-69yjsdh6ez.elasticbeanstalk.com/api/';
+        var Token = Auth.getToken();
 
         return {
-            getResources: function (milliseconds) {
-                var token = Auth.getToken();
+            getResources: function (dateString) {
                 var deferred = $q.defer();
-                if (milliseconds === undefined) {
-                    $http.defaults.headers.common['X-AUTH-TOKEN'] =
-                        token;
-                    $http.get(url + 'resource').success(function (data,
-                        status, headers, config) {
-                        deferred.resolve(data);
-                    })
-                } else {
-                    $http.defaults.headers.common['X-AUTH-TOKEN'] =
-                        token;
-                    console.log(url + 'resource' + '/' + milliseconds.getTime());
-                    $http.get(url + 'resources' + '' /*+milliseconds.getTime()*/ )
-                        .success(function (data, status, headers,
-                            config) {
-                            deferred.resolve(data);
-                        })
+                if(dateString === undefined){
+                  $http.defaults.headers.common['X-AUTH-TOKEN'] = Token;
+                  $http.get(url + 'resources').success(function (data, status, headers, config) {
+                      deferred.resolve(data);
+                  })
+                }
+                else{
+                  $http.defaults.headers.common['X-AUTH-TOKEN'] = Token;
+                  console.log(url + 'resources/'+ dateString);
+                  $http.get(url + 'resources/'+dateString/*+milliseconds.getTime()*/).success(function (data, status, headers, config) {
+                      deferred.resolve(data);
+                  })
                 }
 
                 return deferred.promise
             },
             addTour: function (tourData) {
-                var token = Auth.getToken();
                 var deferred = $q.defer();
-                if (tourData === undefined) {
+                if(tourData === undefined){
                     return;
-                } else {
-                    $http.defaults.headers.common['X-AUTH-TOKEN'] =
-                        token;
-                    $http.post(url + 'tour/' + tourData).success(
-                        function (data, status, headers, config) {
-                            deferred.resolve(data);
-                        })
+                }
+                else{
+                  $http.defaults.headers.common['X-AUTH-TOKEN'] = Token;
+                  $http.post(url + 'tour/'+tourData).success(function (data, status, headers, config) {
+                      deferred.resolve(data);
+                  })
                 }
 
                 return deferred.promise
